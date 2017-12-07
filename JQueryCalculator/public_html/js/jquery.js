65b481a597b8be1af258acb6817a3e2fec73ee02
padoura <padoura@users.noreply.github.com>
2017-12-07 13:55:11 +0200

    Testing ajax jquery method

--- JQueryCalculator/public_html/js/jquery.js
+++ JQueryCalculator/public_html/js/jquery.js
@@ -4,19 +4,19 @@ $("div").append($('<p> </p>'));
 $("p").attr("id","p1");
 $("#p1").append($('<label>Number 1: </label>'));
 $("#p1").append($('<input type="text" value="2"> </input>'));
-$("#p1 input").attr("id","number1");
+$("#p1 > input").attr("id","number1");
 
 $("div").append($('<p></p>'));
 $("#p1").next().attr("id","p2");
 $("#p2").append($('<label>Number 2: </label>'));
 $("#p2").append($('<input type="text" value="2"> </input>'));
-$("#p2 input").attr("id","number2");
+$("#p2 > input").attr("id","number2");
 
 $("div").append($('<p></p>'));
 $("#p2").next().attr("id","p_sign");
 $("#p_sign").append($('<label>Sign: </label>'));
 $("#p_sign").append($('<input type="text"> </input>'));
-$("#p_sign input").attr({"id":"sign","value":"**"});
+$("#p_sign > input").attr({"id":"sign","value":"**"});
 
 $("div").append($('<p></p>'));
 $("#p_sign").next().attr("id","p_calc");
@@ -39,6 +39,31 @@ $("div").css({"display":"table"});
 $("p").css({"display":"table-row"});
 $("label").css({"display":"table-cell"});
 $(".inputClass").css({"display": "table-cell"});
+
+$("div").append('<button>Ajax</button>');
+
+$("body").append('<div id="div1"></div>');
+$("#div1").append('<button>This button will disappear</button>');
+
+
+$("#div1 > button").click(function(){
+    $.ajax({url: "http://echo.jsontest.com/key/value/one/two", success: function(result){
+        $("#div1").html(JSON.stringify(result));
+    }});
+});
+
+//
+//$("#number1").on({
+//        mouseover: function () {
+//            $(this).hide();
+//        }
+//});
+//
+//$("#p_sign").on({
+//        mouseover: function () {
+//            $("#number1").show();
+//        }
+//});
 
 
 
